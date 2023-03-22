ed445b14f4af1fe5f38ce4f207d4326334eb5fde
Jittipat007 <jittipat349@gmail.com>
2023-03-22 18:19:27 +0700

    int103_1.2

--- Myproject1.java
+++ Myproject1.java
@@ -16,6 +16,7 @@ public class Myproject1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hello world");
+        System.out.println("hello java");
     }
     
 }
